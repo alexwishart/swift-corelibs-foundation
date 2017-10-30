30a456a0dc5be379f5b2e71b4ff4778f23ed5246
Alex Wishart <awishart@uk.ibm.com>
2017-10-30 15:02:28 +0000

    Remove code leftover from TestFoundation

--- Benchmarks/main.swift
+++ Benchmarks/main.swift
@@ -21,11 +21,6 @@ import SwiftXCTest
     import Glibc
 #endif
 
-
-internal func testBundle() -> Bundle {
-    return Bundle.main
-}
-
 // For the Swift version of the Foundation tests, we must manually list all test cases here.
 XCTMain([
     testCase(TestJSONSerialization.allTests)
