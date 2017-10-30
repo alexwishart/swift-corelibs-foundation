31772d94e248cdb82f99e44e3b0bfb1395231619
Alex Wishart <awishart@uk.ibm.com>
2017-10-30 13:42:39 +0000

    Adding in Benchmarks target

---
+++ Benchmarks/main.swift
@@ -0,0 +1,32 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+
+#if DEPLOYMENT_RUNTIME_OBJC || os(Linux)
+import Foundation
+import XCTest
+#else
+import SwiftFoundation
+import SwiftXCTest
+#endif
+
+#if os(OSX) || os(iOS)
+    import Darwin
+#elseif os(Linux)
+    import Glibc
+#endif
+
+
+internal func testBundle() -> Bundle {
+    return Bundle.main
+}
+
+// For the Swift version of the Foundation tests, we must manually list all test cases here.
+XCTMain([
+    testCase(TestJSONSerialization.allTests)
+])
